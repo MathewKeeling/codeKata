9fd78386093b3961e562201d2bef03a047e19d6b
Mathew Keeling <soterialogos@protonmail.com>
2022-02-11 20:01:13 -0500

    Kata08 - In Progress... 1/3

--- Kata08 - Conflicting Objectives/_iteration1/conflictingObjectivesOne.py
+++ Kata08 - Conflicting Objectives/_iteration1/conflictingObjectivesOne.py
@@ -14,21 +14,46 @@ def getData(dictionary):
 def getWordsOfLength(words, length):
     wordsOfLength = []
     for word in words:
-        if len(word) <= length:
-            wordsOfLength.append(word)
+        if len(word) >= 2:
+            if len(word) <= length:
+                wordsOfLength.append(word)
     return wordsOfLength
 
+def getWordComposition(word):
+    x = 0
+    compound = []
+    while x <= len(word):
+        if word[0:x+1] in fiveOrLessLetterWords:
+            if word[x+1:] in fiveOrLessLetterWords:
+                string = "First:", {word[0:x+1]} , "Second:", {word[x+1:]}
+                compound.append(string)
+                x = len(word) + 1
+            else:
+                x = x + 1
+        else:
+            x = x + 1
+    return compound
+    
+        
+        
+
 words = getData(".\\resources\wordlist.txt")
-
-
 sixLetterWords = getWordsOfLength(words, 6)
 fiveOrLessLetterWords = getWordsOfLength(words, 5)
 
-print("The Last Ten Six Letter Words: ")
-print(sixLetterWords[-10:])
+#print("The Last Ten Six Letter Words: ")
+#print(sixLetterWords[-10:])
+#print("The Last Ten Five Or Less Letter Words: ")
+#print(fiveOrLessLetterWords[-10:])
 
-print("The Last Ten Five Or Less Letter Words: ")
-print(fiveOrLessLetterWords[-10:])
+print("Proof of concept:")
+testWord = "sixteen"
+test = getWordComposition(testWord)
+print(testWord, "is comprised of: ", test)
+
+sixLetterWordsCompositions = {}
 
 
-    
+for word in sixLetterWords:
+    sixLetterWordsCompositions[word] = getWordComposition(word)
+    print(sixLetterWordsCompositions[word])
