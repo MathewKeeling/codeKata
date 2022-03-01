0f80e30e40a23876d9e72fac6cc0b4b5c3cca40a
Mathew Keeling <soterialogos@protonmail.com>
2022-02-28 20:04:06 -0500

    Kata09 - Refactoring, DRY, Objects

--- Kata09 - Back to the Checkout/checkout.py
+++ Kata09 - Back to the Checkout/checkout.py
@@ -9,40 +9,42 @@ class Product:
         self.discAmt = discAmt
 
     def get_price(self, number_to_be_bought):
+        discQty = self.discQty
+        discAmt = self.discAmt
+        price = self.price
         cost, discount = 0, 0
         discount = number_to_be_bought // discQty * discAmt
         cost = ( price * number_to_be_bought ) + discount
         return cost
 
 def checkout(cart):
+    products = {
+        "a": [50, 3, -20],
+        "b": [30, 2, -15],
+        "c": [20, 1, 0],
+        "d": [15, 1, 0]
+    }
     
-    print("Your total is: ", total)
+    itemQty = [0] * len(products)
 
-products = {
-  "a": [50, 3, -20],
-  "b": [30, 2, -15],
-  "c": [20, 1, 0],
-  "d": [15, 1, 0]
-}
+    totalCost = 0
+    for letter in cart:
+        index = ord(letter) - 96
+        itemQty[index - 1] += 1
+    
+    counter = 0
+    while counter < len(products):
+        item = str(chr(counter + 97))
+        name = item
+        price = products[item][0]
+        discQty = products[item][1]
+        discAmt = products[item][2]
+        itemSelected = Product(name, price, discQty, discAmt)
+        totalCost = totalCost + itemSelected.get_price(itemQty[counter])
+        counter += 1
+    
+    return totalCost
 
-total = 0
-itemQty = [0] * len(products)
-cart = "aaa"
-
-for letter in cart:
-    index = ord(letter) - 96
-    itemQty[index - 1] += 1
-
-x = 0
-while x < len(products):
-    item = str(chr(x + 97))
-    name = item
-    price = products[item][0]
-    discQty = products[item][1]
-    discAmt = products[item][2]
-    productSelected = Product(name, price, discQty, discAmt)
-    total = total + productSelected.get_price(itemQty[x])
-    x += 1
-
-print("Your total is: ", total)
+cart = "aaaaaa"
+print("Your total cost is: ", checkout(cart))
 
