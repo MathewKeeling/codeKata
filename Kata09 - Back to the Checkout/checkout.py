2577c0a8f124e641e67fa8f014d4e927632dc10e
M. Keeling <65958803+MathewKeeling@users.noreply.github.com>
2022-03-07 07:50:37 -0500

    Kata09 - Working on implementing changes suggested
    See M. Sretenovic's notes here:
    https://github.com/MathewKeeling/codeKata/commit/171d606252aa48cbb5cba317c66a6ac2ef2a9a52

--- Kata09 - Back to the Checkout/checkout.py
+++ Kata09 - Back to the Checkout/checkout.py
@@ -2,49 +2,35 @@
 #  https://openbookproject.net/thinkcs/python/english3e/classes_and_objects_I.html
 
 class Product:
-    def __init__(self, name, price, discQty, discAmt):
+    def __init__(self, name, color):
         self.name = name
-        self.price = price
-        self.discQty = discQty
-        self.discAmt = discAmt
+        self.color = color
 
-    def get_price(self, number_to_be_bought):
-        discQty = self.discQty
-        discAmt = self.discAmt
-        price = self.price
-        cost, discount = 0, 0
-        discount = number_to_be_bought // discQty * discAmt
-        cost = ( price * number_to_be_bought ) + discount
-        return cost
+    def print_name(self):
+        print("This object's name is {}".format(self.name))
+        return None
 
-def checkout(cart):
-    products = {
-        "a": [50, 3, -20],
-        "b": [30, 2, -15],
-        "c": [20, 1, 0],
-        "d": [15, 1, 0]
-    }
-    
-    itemQty = [0] * len(products)
+    def print_price(self):
+        print("This object's color is {}".format(self.color))
+        return None
 
-    totalCost = 0
-    for letter in cart:
-        index = ord(letter) - 96
-        itemQty[index - 1] += 1
-    
-    counter = 0
-    while counter < len(products):
-        item = str(chr(counter + 97))
-        name = item
-        price = products[item][0]
-        discQty = products[item][1]
-        discAmt = products[item][2]
-        itemSelected = Product(name, price, discQty, discAmt)
-        totalCost = totalCost + itemSelected.get_price(itemQty[counter])
-        counter += 1
-    
-    return totalCost
+class Price:
+    def __init__(self):
+        self = self
 
-cart = "aaaaaa"
-print("Your total cost is: ", checkout(cart))
+class Discount:
+    def __init__(self):
+        self = self
 
+
+
+
+#  "name": [weight, color]
+products = [ 
+    Product("a", "red"), 
+    Product("b", "orange"), 
+    Product("c", "yellow"), 
+    Product("d", "green") 
+    ]
+
+Product.print_price(products[0])
